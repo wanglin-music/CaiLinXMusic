fb7e072ba1ab39e6292026c06e8c222fd592d7c6
wanglin-music <wanglin-music@thepiratebay.cloud>
2025-02-02 12:03:05 +0630

    Update start.py

--- CaiLinXMusic/plugins/bot/start.py
+++ CaiLinXMusic/plugins/bot/start.py
@@ -35,7 +35,7 @@ async def start_pm(client, message: Message, _):
             keyboard = help_pannel(_)
             await message.reply_sticker("CAACAgUAAxkBAAMFZ448RGxjM8j2H36wdqeXeO1CajMAAo8SAAKb63BXikWOR4jz5Ys2BA")
             return await message.reply_video(
-                photo=config.START_IMG_URL,
+                video="https://files.catbox.moe/ms6cru.mp4",
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
                 reply_markup=keyboard,
             )
@@ -88,7 +88,7 @@ async def start_pm(client, message: Message, _):
         out = private_panel(_)
         await message.reply_sticker("CAACAgUAAxkBAAMFZ448RGxjM8j2H36wdqeXeO1CajMAAo8SAAKb63BXikWOR4jz5Ys2BA")
         await message.reply_video(
-            photo=config.START_IMG_URL,
+            video="https://files.catbox.moe/ms6cru.mp4",
             caption=_["start_2"].format(message.from_user.mention, app.mention),
             reply_markup=InlineKeyboardMarkup(out),
         )
@@ -105,7 +105,7 @@ async def start_gp(client, message: Message, _):
     out = start_panel(_)
     uptime = int(time.time() - _boot_)
     await message.reply_video(
-        photo=config.START_IMG_URL,
+        video="https://files.catbox.moe/ms6cru.mp4",
         caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
         reply_markup=InlineKeyboardMarkup(out),
     )
@@ -140,7 +140,7 @@ async def welcome(client, message: Message):
 
                 out = start_panel(_)
                 await message.reply_video(
-                    photo=config.START_IMG_URL,
+                    video="https://files.catbox.moe/ms6cru.mp4",
                     caption=_["start_3"].format(
                         message.from_user.first_name,
                         app.mention,
